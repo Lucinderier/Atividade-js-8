13ce8df8e24a014877254999609fa9a5b69ce169
Lucinderier <147092779+Lucinderier@users.noreply.github.com>
2024-03-19 15:26:52 -0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,148 @@
+/*
+  01
+
+  - Utilizando function declaration, implemente uma função que recebe 2 números  
+    por parâmetro e retorna o resultado da multiplicação entre esses 2 números;
+  - Previna que esses parâmetros recebam undefined;
+  - Exiba o resultado no console, sem inserir um console.log() dentro da função.
+*/
+function numbers(num1, num2){
+    return num1 * num2
+  }  
+  
+  console.log(numbers(5, 10))
+/*
+  02
+
+  - Faça o mesmo que o exercício acima pede, mas desta vez, implemente uma  
+    **function expression** que retorne o resultado da **divisão** entre esses  
+    2 números.
+*/
+let othernumber = function(num1, num2){
+    return num1 / num2
+  }
+  
+  console.log(othernumber(5, 10))
+/*
+  03
+
+  - Implemente uma função que apenas exibe no console o valor recebido por  
+    parâmetro;
+  - Previna que o parâmetro dessa função receba undefined;
+  - Faça a string abaixo ser exibida 7x no console;
+  - A cada exibição, substitua o "X" pela informação correta;
+  - Não repita (manualmente) a invocação da função ou do console.log().
+
+  "Esta é a Xª vez que essa string é exibida."
+*/
+let value = function(value = 1){
+    console.log(value)
+    for (i = value; i <= 7; i++){
+      console.log(`Esta é a ${i}ª vez que essa string é exibida.`)
+    }
+  }
+  
+  console.log(value(1))
+/*
+  04
+
+  - Comente o código acima, de forma que a string não seja mais exibida no  
+    console;
+  - Implemente uma função que retorna um novo array com as strings do array  
+    "millennialWords" em letras maiúsculas;
+  - Exiba o novo array no console, sem inserir um console.log() dentro da  
+    função.
+*/
+
+const millennialWords = ['lol', 'yolo', 'troll', 'stalkear', 'selfie', 'influencer', 'crush', 'fitness', 'hater', 'bae', 'random', 'kawaii', 'outfit', 'mood', 'fail'];
+
+const conversão = (array) => array.map(word => word.toUpperCase());
+
+let resultado = conversão(millennialWords);
+console.log(resultado);
+/*
+  05
+
+  - Implemente uma função que retorna se um número é positivo;
+  - Use essa função para descobrir quantos números positivos o array 
+    "randomNumbers" possui;
+  - Exiba a frase abaixo no console, inserindo as informações corretas.
+
+  "O array "randomNumbers" possui XX números, sendo XX positivos e XX negativos."
+*/
+
+function count(numeros) {
+    let contadorPositivos = 0;
+    let contadorNegativos = 0;
+  
+    for (let i = 0; i < numeros.length; i++) {
+      if (numeros[i] > 0) {
+        contadorPositivos++;
+      } else if (numeros[i] < 0) {
+        contadorNegativos++;
+      }
+    }
+  
+    return {positivos: contadorPositivos, negativos: contadorNegativos};
+  }
+  
+  const randomNumbers = [-2, 93, 34, -1, 1, 93, 11, -7, 47, -3];
+  const { positivos, negativos } = count(randomNumbers);
+  
+  console.log(`O array "randomNumbers" possui ${randomNumbers.length} números, sendo ${positivos} positivos e ${negativos} negativos.`);
+  
+
+/*
+  06
+
+  - Descomente a invocação da função abaixo e implemente-a;
+  - Ela deve retornar um novo array com apenas os números ímpares do array 
+    passado por argumento;
+  - Exiba o novo array no console, sem inserir um console.log() dentro da  
+    função.
+*/
+
+const returnImpar = array => array.filter(num => num % 2 !== 0);
+
+const getOddNumbers = [83, 52, 31, 73, 98, 37, 61, 56, 12, 24, 35, 3, 34, 80, 42];
+const arrayImpares = returnImpar(getOddNumbers);
+console.log(arrayImpares);
+
+/*
+  07
+
+  - Forme uma frase com o array abaixo e exiba-a no console.
+*/
+
+const functions = [
+  function () { return 'Plymouth' },
+  function () { return 'é' },
+  function () { return 'uma' },
+  function () { return 'cidade' },
+  function () { return 'fantasma' },
+  function () { return 'localizada' },
+  function () { return 'na' },
+  function () { return 'ilha' },
+  function () { return 'de' },
+  function () { return 'Montserrat,' },
+  function () { return 'um' },
+  function () { return 'território' },
+  function () { return 'ultramarino' },
+  function () { return 'do' },
+  function () { return 'Reino' },
+  function () { return 'Unido' },
+  function () { return 'localizado' },
+  function () { return 'na' },
+  function () { return 'cadeia' },
+  function () { return 'de' },
+  function () { return 'Ilhas' },
+  function () { return 'de' },
+  function () { return 'Sotavento' },
+  function () { return 'nas' },
+  function () { return 'Pequenas' },
+  function () { return 'Antilhas,' },
+  function () { return 'Índias' },
+  function () { return 'Ocidentais.' }
+]
+const phrase = functions.map(functions => functions()).join(' ')
+console.log(phrase)
